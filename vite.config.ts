c6ebd316071ebdf4d031611482f644014a1427cf
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2026-02-14 14:14:05 +0530

    removed the base url which i added before

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,6 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  base: "/IgniteX-SL/sentinel-watch-ignitex",
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean,
   ),
